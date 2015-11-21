95c9618b98be739807b50dcb3ed91a4379efbb7f
Andrew Head <head.andrewm@gmail.com>
2015-11-21 15:26:54 -0800

    Prototypes: #7 Simplex optimization front-end works!  With grayscale image

--- proto/p7/simplex/simplex/static/simplex/js/ui.js
+++ proto/p7/simplex/simplex/static/simplex/js/ui.js
@@ -1,47 +1,7 @@
-/*global d3*/
+/*global d3, $*/
 /*jslint unparam:true */
 
-console.log("Hello world!");
-
-function color(d) { 
-  var v = d.value;
-  return 'rgb(' + [v, v, v].join(',') + ')'; 
-}
-
-var exemplarSvg = d3.select('#exemplar_cont')
-  .append('svg')
-  .attr('width', 300)
-  .attr('height', 300);
-
-exemplarSvg.selectAll('rect')
-  .data([{'value': 128}])
-  .enter()
-    .append('rect')
-      .attr('width', 300)
-      .attr('height', 300)
-      .style('fill', color);
-
-var boxData = [
-  {'rank': 1, 'value': 128}, 
-  {'rank': 2, 'value': 64}, 
-  {'rank': 3, 'value': 100}, 
-  {'rank': 4, 'value': 32}
-];
-
-var rankSvg = d3.select('#rank_bar')
-  .append('svg')
-  .attr('width', 100 * boxData.length)
-  .attr('height', 100);
-
-var boxes = rankSvg.selectAll('rect')
-  .data(boxData)
-  .enter()
-    .append('rect')
-      .attr('x', function(d, i) { return i * 100; } )
-      .attr('width', 100)
-      .attr('height', 100)
-      .style('fill', color);
-
+/* HELPERS */
 
 // REUSE: http://stackoverflow.com/questions/14167863/how-can-i-bring-a-circle-to-the-front-with-d3
 d3.selection.prototype.moveToFront = function() {
@@ -50,9 +10,31 @@ d3.selection.prototype.moveToFront = function() {
   });
 };
 
+function color(d) { 
+  var v = Math.floor(d.value[0]);
+  return 'rgb(' + [v, v, v].join(',') + ')'; 
+}
+
+/* DISPLAY THE GOAL */
+
+var exemplarSvg = d3.select('#exemplar_cont')
+  .append('svg')
+  .attr('width', 200)
+  .attr('height', 200);
+
+exemplarSvg.selectAll('rect')
+  .data([{'value': [128]}])
+  .enter()
+    .append('rect')
+      .attr('width', 200)
+      .attr('height', 200)
+      .style('fill', color);
+
+/* MANIPULATE THE EXAMPLES */
+
 function getDisplacedNeighbor(sel) {
     var displaced = d3.selectAll('.nothing');
-    boxes.each(function() {
+    d3.selectAll('#rank_bar rect').each(function() {
       var neighbor = d3.select(this);
       if (neighbor.node() !== sel.node()) {
         if (Math.abs(neighbor.attr('x') - sel.attr('x')) < 50) {
@@ -78,7 +60,7 @@ var drag = d3.behavior.drag()
 
     // Highlight nearby box it will replace
     var displaced = getDisplacedNeighbor(selection);
-    boxes.style('stroke-width', 0);
+    d3.selectAll('#rank_bar rect').style('stroke-width', 0);
     displaced
       .style('stroke-width', 3)
       .style('stroke', 'rgb(166, 166, 255)');
@@ -87,18 +69,17 @@ var drag = d3.behavior.drag()
     
     // Rearrange the boxes based on where the current selection is dragged
     var selection = d3.select(this);
-    var origX = selection.attr('x');
+    var origX = selection.datum().origX;
     var displaced = getDisplacedNeighbor(selection);
     var newX = displaced.attr('x');
+    console.log("Original X:" + origX);
+    console.log("New X:" + newX);
     selection.attr('x', newX);
     displaced.attr('x', origX);
 
-    boxes.each(function() {
-      var thisNode = d3.select(this);
-      thisNode.datum(function(d) {
-        d.x = thisNode.attr('x');
-        return d;
-      });
+    var boxes = d3.selectAll('#rank_bar rect');
+    boxes.each(function(d, i) {
+      d.x = $(this).attr('x');
     });
     boxes.sort(function(a, b) {
       return a.x - b.x;
@@ -106,7 +87,119 @@ var drag = d3.behavior.drag()
     boxes
       .attr('x', function(d, i) { return i * 100; })
       .attr('y', 0)
+      .datum(function(d, i) { 
+        d.origX = d3.select(this).attr('x');
+        d.rank = i;
+        return d; 
+      })
       .style('stroke-width', 0);
 
   });
-boxes.call(drag);
+
+/* LOAD THE EXAMPLES */
+
+function makeData(vertices, extras) {
+    var i;
+    var data = [];
+    for (i = 0; i < vertices.length; i++) {
+        data.push({'rank': i, 'value': vertices[i], 'type': 'vertex'});
+    }
+    if (extras !== undefined) {
+        for (i = 0; i < extras.length; i++) {
+            data.push({
+                'rank': vertices.length + i, 
+                'value': extras[i].value,
+                'type': extras[i].type
+            });
+        }
+    }
+    return data;
+}
+
+function loadExamples(vertices, extras) {
+
+    var data = makeData(vertices, extras);
+
+    d3.select('#rank_bar svg').remove();
+    var rankSvg = d3.select('#rank_bar')
+      .append('svg')
+      .attr('width', 100 * data.length)
+      .attr('height', 100);
+
+    var boxes = rankSvg.selectAll('rect')
+      .data(data)
+      .enter()
+        .append('rect')
+          .attr('x', function(d, i) { return i * 100; } )
+          .datum(function(d, i) { 
+            d.origX = i * 100;
+            return d; 
+          }).attr('width', 100)
+          .attr('height', 100)
+          .datum(function(d) { 
+            d.origX = d3.select(this).attr('x');
+            return d; 
+          })
+          .style('fill', color)
+          .call(drag);
+
+    return boxes;
+}
+
+var boxes = loadExamples([[255], [172], [100], [32]]);
+
+$('#upload_ranking_butt').click(function() {
+
+    function getDataOfType(data, type) {
+        var i;
+        var typedData = [];
+        for (i = 0; i < data.length; i++) {
+            if (data[i].type === type) {
+                typedData.push(data[i]);
+            }
+        }
+        return typedData;
+    }
+
+    var allData = d3.selectAll('#rank_bar rect').data();
+    var vData = getDataOfType(allData, 'vertex');
+
+    function values(data) { 
+        return data.map(function (e) { 
+            return e.value; 
+        });
+    }
+    function ranks(data) { 
+        return data.map(function (e) { 
+            return e.rank; 
+        });
+    }
+    function rankOfType(data, type) {
+        return ranks(getDataOfType(data, type))[0];
+    }
+
+    if (vData.length === allData.length) {
+        $.get('/get_next', {
+            'vertices': JSON.stringify(values(vData)),
+            'vertex_ranks': ranks(vData),
+        }, function(data) {
+            var origData = values(vData);
+            loadExamples(origData, [
+                {'value': data.reflection, 'type': 'reflection'},
+                {'value': data.expansion, 'type': 'expansion'},
+                {'value': data.contraction, 'type': 'contraction'},
+            ]);       
+        });
+    } else {
+        $.get('/update_vertices', {
+            'vertices': JSON.stringify(values(vData)),
+            'vertex_ranks': ranks(vData),
+            'reflection_rank': rankOfType(allData, 'reflection'),
+            'expansion_rank': rankOfType(allData, 'expansion'),
+            'contraction_rank': rankOfType(allData, 'contraction'),
+        }, function(data) {
+            loadExamples(data.vertices);
+        });
+    }
+
+});
--- proto/p7/simplex/simplex/views.py
+++ proto/p7/simplex/simplex/views.py
@@ -3,11 +3,16 @@
 
 from __future__ import unicode_literals
 import logging
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
+import numpy as np
+import json
+
+from simplex import SimplexExecutor
 
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
+simplex_executor = SimplexExecutor()
 
 
 def home(request):
@@ -15,8 +20,33 @@ def home(request):
 
 
 def update_vertices(request):
-    return HttpResponse("Hello world!")
+    print request.GET
+    print np.array(json.loads(request.GET.get('vertices'))),
+    print np.array(request.GET.getlist('vertex_ranks[]')),
+    print request.GET.get('reflection_rank'),
+    print request.GET.get('expansion_rank'),
+    print request.GET.get('contraction_rank'),
+    updated_vertices = simplex_executor.update_points(
+        np.array(json.loads(request.GET.get('vertices'))),
+        np.array([int(_) for _ in request.GET.getlist('vertex_ranks[]')]),
+        int(request.GET.get('reflection_rank')),
+        int(request.GET.get('expansion_rank')),
+        int(request.GET.get('contraction_rank')),
+    )
+    return JsonResponse({
+        'vertices': updated_vertices.tolist(),
+    })
 
 
 def get_next(request):
-    return HttpResponse("Hello world!")
+    vertices = json.loads(request.GET.get('vertices'))
+    vertex_ranks = request.GET.getlist('vertex_ranks[]')
+    reflection, expansion, contraction = simplex_executor.get_next_points(
+        np.array(vertices),
+        np.array(vertex_ranks),
+    )
+    return JsonResponse({
+        'reflection': reflection.tolist(),
+        'expansion': expansion.tolist(),
+        'contraction': contraction.tolist(),
+    })
