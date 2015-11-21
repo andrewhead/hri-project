2a63406dc7d386061c2cd01ace894e41db76ca56
Andrew Head <head.andrewm@gmail.com>
2015-11-21 12:58:40 -0800

    Prototypes: #7 front-end allows re-ranking the boxes

--- proto/p7/simplex/simplex/static/simplex/js/ui.js
+++ proto/p7/simplex/simplex/static/simplex/js/ui.js
@@ -3,10 +3,23 @@
 
 console.log("Hello world!");
 
-var svg = d3.select('#rank_panel')
+function color(d) { 
+  var v = d.value;
+  return 'rgb(' + [v, v, v].join(',') + ')'; 
+}
+
+var exemplarSvg = d3.select('#exemplar_cont')
   .append('svg')
-  .attr('width', 400)
-  .attr('height', 400);
+  .attr('width', 300)
+  .attr('height', 300);
+
+exemplarSvg.selectAll('rect')
+  .data([{'value': 128}])
+  .enter()
+    .append('rect')
+      .attr('width', 300)
+      .attr('height', 300)
+      .style('fill', color);
 
 var boxData = [
   {'rank': 1, 'value': 128}, 
@@ -15,27 +28,71 @@ var boxData = [
   {'rank': 4, 'value': 32}
 ];
 
-var boxes = svg.selectAll('rect')
+var rankSvg = d3.select('#rank_bar')
+  .append('svg')
+  .attr('width', 100 * boxData.length)
+  .attr('height', 100);
+
+var boxes = rankSvg.selectAll('rect')
   .data(boxData)
   .enter()
     .append('rect')
-    .attr('x', function(d, i) { return i * 100; } )
-    .attr('width', 100)
-    .attr('height', 100)
-    .style('fill', function(d) { 
-      var v = d.value;
-      return 'rgb(' + [v, v, v].join(',') + ')'; 
+      .attr('x', function(d, i) { return i * 100; } )
+      .attr('width', 100)
+      .attr('height', 100)
+      .style('fill', color);
+
+
+// REUSE: http://stackoverflow.com/questions/14167863/how-can-i-bring-a-circle-to-the-front-with-d3
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function(){
+    this.parentNode.appendChild(this);
+  });
+};
+
+function getDisplacedNeighbor(sel) {
+    var displaced = d3.selectAll('.nothing');
+    boxes.each(function() {
+      var neighbor = d3.select(this);
+      if (neighbor.node() !== sel.node()) {
+        if (Math.abs(neighbor.attr('x') - sel.attr('x')) < 50) {
+           displaced = neighbor;
+        }
+      }
     });
+    return displaced;
+}
 
-
+/* Drag and snap boxes within a line */
 // REUSE: Based on drag behavior from
 // stackoverflow.com/questions/19931307/d3-differentiate-between-click-and-drag-for-an-element-which-has-a-drag-behavior
 var drag = d3.behavior.drag()
   .on('drag', function() {
-    d3.select(this)
+
+    // Move the box
+    var selection = d3.select(this);
+    selection
       .attr('x', d3.event.x - d3.select(this).attr('width') / 2)
-      .attr('y', d3.event.y - d3.select(this).attr('height') / 2);
+      .attr('y', d3.event.y - d3.select(this).attr('height') / 2)
+      .moveToFront();
+
+    // Highlight nearby box it will replace
+    var displaced = getDisplacedNeighbor(selection);
+    boxes.style('stroke-width', 0);
+    displaced
+      .style('stroke-width', 3)
+      .style('stroke', 'rgb(166, 166, 255)');
+
   }).on('dragend', function() {
+    
+    // Rearrange the boxes based on where the current selection is dragged
+    var selection = d3.select(this);
+    var origX = selection.attr('x');
+    var displaced = getDisplacedNeighbor(selection);
+    var newX = displaced.attr('x');
+    selection.attr('x', newX);
+    displaced.attr('x', origX);
+
     boxes.each(function() {
       var thisNode = d3.select(this);
       thisNode.datum(function(d) {
@@ -46,6 +103,10 @@ var drag = d3.behavior.drag()
     boxes.sort(function(a, b) {
       return a.x - b.x;
     });
-    boxes.attr('x', function(d, i) { return i * 100; });
+    boxes
+      .attr('x', function(d, i) { return i * 100; })
+      .attr('y', 0)
+      .style('stroke-width', 0);
+
   });
 boxes.call(drag);
